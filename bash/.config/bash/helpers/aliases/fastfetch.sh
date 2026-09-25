c217e6bde690a6431bb0a4fa7eeb2561670b61ac
Tyler Dalton <tldalton54@gmail.com>
2026-09-25 15:21:00 -0400

    feat(helpers): fastfetch (K,M)
    Signed-off-by: Tyler Dalton <tldalton54@gmail.com>

--- bash/.config/bash/helpers/aliases/fastfetch.sh
+++ bash/.config/bash/helpers/aliases/fastfetch.sh
@@ -12,38 +12,69 @@ alias ffll='fastfetch --list-logos'
 # MISC.
 alias ff-tux='fastfetch --logo altlinux'
 alias ff-tux2='fastfetch --logo calculate'
+alias ff-tux3='fastfetch --logo linux'
+alias ff-tux4='fastfetch --logo lfs'
+alias ff-tux5='fastfetch --logo locos'
 alias ff-crux='fastfetch --logo crux'
 # ALMA
 alias ff-alma='fastfetch --logo almalinux'
 # APPLE
 alias ff-apple='fastfetch --logo apple'
+alias ff-apple2='fastfetch --logo macos2'
+alias ff-apple3='fastfetch --logo macos3'
 # ARCH
 alias ff-arch='fastfetch --logo arch'
 alias ff-arch2='fastfetch --logo arch2'
 alias ff-arch3='fastfetch --logo archcraft'
 alias ff-artix='fastfetch --logo artix'
 
-
+# BEDROCK
 alias ff-bedrock='fastfetch --logo bedrock'
+# BLACK ARCH
 alias ff-blackarch='fastfetch --logo blackarch'
+# BONSAI
 alias ff-bonsai='festfetch --logo bonsai'
+# BSD
 alias ff-bsd='fastfetch --logo bsd'
 
+
+# CACHYOS
 alias ff-cachy='fastfetch --logo cachyos'
+# CHROME
 alias ff-chrome='fastfetch --logo chrom'
 
+
+# DEBIAN
 alias ff-debian='fastfetch --logo debian'
 
 
+# FASTFETCH
 alias ff-ff='fastfetch --logo ff'
+# FEDORA
 alias ff-fedora='fastfetch --logo fedora'
+# FREE BSD
 alias ff-freebsd='fastfetch --logo freebsd'
 
 
+# GENTOO
 alias ff-gentoo='fastfetch --logo gentoo'
+# GHOST FREAK
 alias ff-ghotst='fastfetch --logo ghostfreak'
+# GNOME
 alias ff-gnome='fastfetch --logo gnome'
+# GNU
 alias ff-gnu='fastfetch --logo gnu'
 
+
+# KALI
 alias ff-kali='fastfetch --logo kali'
+# KDE-LINUX
 alias ff-kde='fastfetch --logo kde-linux'
+
+
+# LINUX MINT
+alias ff-mint='fastfetch --logo linuxmint'
+# MANJARO
+alias ff-manjaro='fastfetch --logo manjaro'
+# MX LINUX
+alias ff-mx='fastfetch --logo mx'
