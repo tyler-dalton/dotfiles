b3bef1a8b00bdac8fb69eca06d8f49a907d8b4db
Tyler Dalton <tldalton54@gmail.com>
2026-09-25 12:58:49 -0400

    feat(helpers): base flatpak helpers
    Signed-off-by: Tyler Dalton <tldalton54@gmail.com>

---
+++ bash/.config/bash/helpers/aliases/flatpak.sh
@@ -0,0 +1,9 @@
+# =============
+# FLATPAK - tld 9.24.26
+# =============
+
+alias flt='flatpak'
+alias flti='flatpak install'
+alias fltrem='flatpak remove'
+alias fltu='flatpak update'
+alias fltlist='flatpak list'
