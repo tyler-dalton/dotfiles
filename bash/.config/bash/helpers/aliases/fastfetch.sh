bfde76272a1441f3657f3607a7820b5f23525a48
Tyler Dalton <tldalton54@gmail.com>
2026-09-25 13:14:48 -0400

    feat(helpers): aliases (A)
    Signed-off-by: Tyler Dalton <tldalton54@gmail.com>

---
+++ bash/.config/bash/helpers/aliases/fastfetch.sh
@@ -0,0 +1,23 @@
+# =============
+# FASTFETCH - tld 9.24.26
+# =============
+
+alias ff='fastfetch'
+alias ffl='fastfetch --logo'
+alias ffll='fastfetch --list-logos'
+alias ffgrep='fastfetch --list-logos | grep'
+
+# QUICK LOGOS
+# -------------
+
+# MISC.
+alias ff-tux='fastfetch --logo ALTLinux'
+# ALMA
+alias ff-alma='fastfetch --logo almalinux'
+# APPLE
+alias ff-apple='fastfetch --logo apple'
+# ARCH
+alias ff-arch='fastfetch --logo arch'
+alias ff-arch2='fastfetch --logo arch2'
+alias ff-arch3='fastfetch --logo ArchCraft'
+alias ff-artix='fastfetch --logo artix'
